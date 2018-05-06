1474de34417638cd1cd6435dafd3764fbdbd886f
VG1349 <36368691+VG1349@users.noreply.github.com>
2018-05-06 19:11:06 +0300

    Add files via upload

---
+++ app-node.js
@@ -0,0 +1,21 @@
+var http_port = 3000;
+
+var http    = require('http');
+var htutil  = require('./htutil');
+
+var server = http.createServer(function (req, res) {
+    htutil.loadParams(req, res, undefined);
+    if (req.requrl.pathname === '/') {
+        require('./home-node').get(req, res);
+    } else if (req.requrl.pathname === '/factorial') {
+        require('./factorial-node').get(req, res);
+    } else if (req.requrl.pathname === '/fibonacci') {
+        require('./fibo-node').get(req, res);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end("bad URL "+ req.url);
+    }
+});
+
+server.listen(http_port);
+console.log('listening to http://localhost:3000');
